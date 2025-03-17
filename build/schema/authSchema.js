4cd47bf53c7b02de71b940c58c461cf3bd0146ab
Sidharth Tripathi <sidharthtripathi807@gmail.com>
2025-03-17 22:04:55 +0530

    removed build folder from git

--- build/schema/authSchema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authSchema = void 0;
-const zod_1 = require("zod");
-exports.authSchema = zod_1.z.object({
-    username: zod_1.z.string(),
-    password: zod_1.z.string()
-});
